a2e9fae53c4f6da4f5cc248ba1c4d749b718ca3f
MamytovKubatbek <kubatbekmamytov0000.com>
2022-09-28 17:09:54 +0200

    full_project

--- setting_app/views.py
+++ setting_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def main(request):
-    return render(request, 'index.html') 
+    category = Category.objects.all()
+    meals = Meals.objects.all()
+    context = {
+        'meals': meals, 
+        'category': category,
+    
+    }
+    return render(request, 'index.html', context=context)
+
+
